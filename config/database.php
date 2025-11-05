0604985f7d5166533154d5799a1812a4e0d8181a
SebastianAc02 <sacostamolina@outlook.com>
2025-11-05 17:32:04 -0500

    Fix PostgreSQL config for Railway

--- config/database.php
+++ config/database.php
@@ -16,6 +16,8 @@ return [
     |
     */
 
+
+
     'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
@@ -85,7 +87,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'), // ← ESTO ES CRÍTICO para Railway
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
